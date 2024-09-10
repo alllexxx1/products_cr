c22a0244327af0d61ff86d14c40d4ff784dbe987
alllexxx1 <alekkorpachyov@gmail.com>
2024-09-10 16:10:16 +0300

    Add the Product model to the project with basic documentation. Create migrations for DB

--- products_cr/products/models.py
+++ products_cr/products/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    """
+    The model describes the only existing table in the project.
+    It represents a product in the system and contains 3 basic fields.
+
+    """
+
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        """
+        Return the string representation of the Product instances.
+        """
+        return self.name
